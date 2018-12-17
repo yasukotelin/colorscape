41b3cd28ccd781c7e9b4e2cedeccaaa961fe9523
yasuhiro.kato <yasuhiro.kato@cct-inc.co.jp>
2018-12-17 18:11:10 +0900

    Update FlagにHexを追加した

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/urfave/cli"
@@ -10,6 +12,7 @@ const (
 	appName = "colorscape"
 	usage   = "colorscape is a color code conversion tool."
 	version = "0.0.1"
+	author  = "ysbrothersk"
 )
 
 func main() {
@@ -18,6 +21,21 @@ func main() {
 	app.Name = appName
 	app.Usage = usage
 	app.Version = version
+	app.Author = author
 
-	app.Run(os.Args)
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "hex, x",
+			Usage: "Hex color code",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Println(c.String("hex"))
+		return nil
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
