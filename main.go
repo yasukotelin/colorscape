014f60599c5573442b62a2bfb5a5697780297a4a
yasuhiro.kato <yasuhiro.kato@cct-inc.co.jp>
2018-12-21 15:17:45 +0900

    refactor: codeのaction関数化

--- main.go
+++ main.go
@@ -28,23 +28,25 @@ func main() {
 	app.Commands = []cli.Command{
 		{
 			Name:    "code",
-			Aliases: []string{"c", "hex"},
+			Aliases: []string{"c", "hex", "h"},
 			Usage:   "convert to rgb from hex color code",
-			Action: func(c *cli.Context) error {
-				code := c.Args().First()
-				rgb, err := conv.ToRgb(code)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				fmt.Println(rgb[0], rgb[1], rgb[2])
-
-				return nil
-			},
+			Action:  codeAction,
 		},
 	}
 
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func codeAction(c *cli.Context) error {
+	code := c.Args().First()
+	rgb, err := conv.ToRgb(code)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(rgb[0], rgb[1], rgb[2])
+
+	return nil
 }
